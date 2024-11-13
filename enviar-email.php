79aa2a019a37985a19c4c139db6800051d34a3ed
ramonchuinca <ramonchuinca@hotmail.com>
2024-11-13 17:07:31 -0400

    envio de e-mail funcional

--- enviar-email.php
+++ enviar-email.php
@@ -1,20 +1,23 @@
 <?php
+header('/');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $mensagem = $_POST['mensagem'];
 
-    $to = "ramonchuinca@hotmail.com";
+    $to = "";
     $subject = "Nova mensagem do formulário de contato";
     $body = "Nome: $nome\nEmail: $email\n\nMensagem:\n$mensagem";
     $headers = "From: $email";
 
     if (mail($to, $subject, $body, $headers)) {
+        header('/');
         echo "Mensagem enviada com sucesso!";
     } else {
+        header('/');
         echo "Erro ao enviar mensagem.";
     }
 }
-?>
+
 
 
